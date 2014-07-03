bae5141770bc6667a6fcde79090f6db62d15f965
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2014-07-03 23:17:59 +0000

    oo friendlier
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/site/trunk@67 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- lib/view.pm
+++ lib/view.pm
@@ -2,7 +2,7 @@ package view;
 use base 'ASF::View';
 sub set_template {
     my %args = @_;
-    my $view = ASF::View::next_view \%args;
+    my $view = view->next_view(\%args);
     $args{template} = "$1.html";
     return view->can($view)->(%args);
 }
