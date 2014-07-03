2194740bf42760cf32ea59f680abee28c8116260
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2014-07-03 04:53:32 +0000

    sitemap deps
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/site/trunk@10 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- lib/path.pm
+++ lib/path.pm
@@ -4,9 +4,21 @@ use strict;
 use warnings;
 
 our @patterns = (
-    [qr!^/sitemap\.html$!, sitemap => { headers => {title => "Sitemap"}, nest => 1 }],
+    [qr!^/sitemap\.html$!, sitemap => {
+        headers    => {title => "Sitemap"},
+        nest       => 1,
+        quick_deps => 1,
+    }],
     [qr/\.md(?:text)?$/,  single_narrative => { template => "main.html" }],
 );
+
+our %dependencies;
+
+walk_content_tree {
+    if (/\.md(?:text)?$/ or m!/index\.html$!) {
+        push @{$dependencies{"/sitemap.html"}}, $_;
+    }
+}
 
 
 1;
