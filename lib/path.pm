2123370ec9c33f982e694137fbe8ecf92cea4ab9
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2014-07-06 05:25:13 +0000

    ezmlm
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/site/trunk@119 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- lib/path.pm
+++ lib/path.pm
@@ -41,7 +41,7 @@ walk_content_tree {
                                glob("content$_/*/index.html")
         ];
         push @{$dependencies{"$_/index.html"}, grep s/^content//,
-                   glob "content$_/*" if m!/files$!;
+                   glob("content$_/*") if m!/files$!;
     }
 };
 
