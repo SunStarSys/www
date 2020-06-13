60518974a9d4e61bbc1900b4b8fd5255d0cffe89
(no author) <(no author)@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2020-06-13 06:28:28 +0000

    rebuild
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/site/trunk@738 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- lib/path.pm
+++ lib/path.pm
@@ -51,6 +51,7 @@ push @{$dependencies{"/essays/files/index.html"}}, grep -f && s/^content// && !m
 
 1;
 
+
 __DATA__
 title: "SunStar Systems"
 keywords: "sysadmin,sunstar systems,perl,mod_perl,c,xs,httpd,apache,puppet,qmail,ezmlm,git,subversion,mysql,postgresql,linux,freebsd,solaris,nagios"
