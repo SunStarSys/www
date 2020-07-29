7648c8b1f259d271f77aa82a36885f25d2845b1c
(no author) <(no author)@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2020-07-29 22:25:39 +0000

    CMS commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/site/trunk@2400 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- lib/path.pm
+++ lib/path.pm
@@ -52,7 +52,6 @@ push @{$dependencies{"/essays/files/index.html"}}, grep -f && s/^content// && !m
 
 1;
 
-
 __DATA__
 title: "SunStar Systems"
 keywords: "perl,mod_perl,mod_apreq2,c,xs,httpd,apache,puppet,qmail,ezmlm,git,subversion,mysql,postgresql,linux,freebsd,solaris,devops"
