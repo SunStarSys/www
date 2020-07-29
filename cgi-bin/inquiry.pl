b502b2a5f293dd3ddbc2114b7a89c0412de5cfef
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2020-07-29 03:43:00 +0000

    inquiry
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/site/trunk@2156 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/inquiry.pl
+++ cgi-bin/inquiry.pl
@@ -28,7 +28,7 @@ if ($ENV{REQUEST_METHOD} eq "POST") {
     s/\r//g for $email, $subject, $content;
     s/\n//g for $email, $subject;
 
-    s/\s*(\S+\@\S+)\s*$// for my $cn = $email;
+    s/\s*[<(]?(\S+\@\S+)[)>]?\s*$// for my $cn = $email;
     my $srs_sender = $1;
     for ($cn, $subject) {
         if (s/([^^A-Za-z0-9\-_.,!~*' ])/sprintf "=%02X", ord $1/ge) {
@@ -37,7 +37,7 @@ if ($ENV{REQUEST_METHOD} eq "POST") {
         }
     }
 
-    $srs_sender =~ s/^(.*)\@(.*)$/SRS0=999=99=$2=$1/;
+    s/^(.*)\@(.*)$/SRS0=999=99=$2=$1/;
 
     local %ENV;
     open my $sendmail, "|-", "/usr/sbin/sendmail -oi -t -f '$srs_sender\@$DOMAIN'"
