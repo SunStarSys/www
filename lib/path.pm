a61b38a12c31b48c40c3c66c3a05d498bc953098
(no author) <(no author)@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2020-09-03 16:48:47 +0000

    CMS commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@3697 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- lib/path.pm
+++ lib/path.pm
@@ -50,6 +50,7 @@ for my $lang (qw/en es de/) {
 
 1;
 
+
 __DATA__
 title: "SunStar Systems"
 keywords: "perl,mod_perl,mod_apreq2,c,xs,httpd,apache,puppet,qmail,ezmlm,git,subversion,mysql,postgresql,linux,freebsd,solaris,devops"
