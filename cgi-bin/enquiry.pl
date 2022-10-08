702bc053f85ce7d96be9078f97a2deb528b63217
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2022-10-08 16:06:46 +0000

    CMS commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/branches/zen@5206 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/enquiry.pl
+++ cgi-bin/enquiry.pl
@@ -40,21 +40,21 @@ if ($r->method eq "POST") {
 
     my ($cn, $srs_sender) = ($name, $email);
 
-    for ($cn, $subject) {
+    for ($cn, $subject) {\
         if (s/([^^A-Za-z0-9\-_.,!~*' ])/sprintf "=%02X", ord $1/ge) {
             tr/ /_/;
             $_ = "=?utf-8?Q?$_?=";
         }
     }
 
-	if ($subject =~ /^cms/i) {
-    	s/^(.*)\@(.*)$/SRS0=999=99=$2=$1/, y/A-Za-z0-9._=-//dc for $srs_sender;
-		$srs_sender =~ /(.*)/;
-		length $1 or die "BAD EMAIL: $email";
-    	%ENV = ();
+    if ($subject =~ /^cms/i) {
+        s/^(.*)\@(.*)$/SRS0=999=99=$2=$1/, y/A-Za-z0-9._=-//dc for $srs_sender;
+        $srs_sender =~ /(.*)/;
+        length $1 or die "BAD EMAIL: $email";
+        %ENV = ();
 
-		open my $sendmail, "|-", "/usr/sbin/sendmail", qw/-t -oi -odq -f/, "$1\@$DOMAIN";
-   		print $sendmail <<EOT;
+        open my $sendmail, "|-", "/usr/sbin/sendmail", qw/-t -oi -odq -f/, "$1\@$DOMAIN";
+        print $sendmail <<EOT;
 To: $to
 From: $cn <$srs_sender\@$DOMAIN>
 Reply-To: $cn <$email>
@@ -69,10 +69,10 @@ HOSTING: $hosting
 LANGUAGE: $plang
 EOT
 
-   		close $sendmail or die "sendmail failed: " . ($! || $? >> 8) . "\n";
-	}
+        close $sendmail or die "sendmail failed: " . ($! || $? >> 8) . "\n";
+    }
 
-	render $r, "enquiry_post.html",
+    render $r, "enquiry_post.html",
         content => "## Thank You!\n\nOur Sales Team will get back to you shortly.\n",
         headers => { title => "CMS Sales Enquiry" };
 }
