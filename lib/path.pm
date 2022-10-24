b4fa56b311553be56448947082c53b5a3241f923
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2022-10-24 03:05:21 +0000

    whitespace
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@5260 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- lib/path.pm
+++ lib/path.pm
@@ -7,54 +7,54 @@ use File::Path 'mkpath';
 my $conf = Load join "", <DATA>;
 
 our @patterns = (
-    [qr!/(index|sitemap)\.html!, sitemap => {
-        quick_deps    => 1,
-        nest          => 1,
-        conf          => $conf,
-    }],
-    [qr!^/(essay|client)s/.*\.md(?:text)?!,  set_template_from_capture => {
-        view       => "single_narrative",
-        preprocess => 1,
-        conf       => $conf,
-    }],
-    [qr/\.md(?:text)?/,  single_narrative => {
-        template   => "main.html",
-        preprocess => 1,
-        conf       => $conf,
-    }],
+  [qr!/(index|sitemap)\.html!, sitemap => {
+    quick_deps    => 1,
+    nest          => 1,
+    conf          => $conf,
+  }],
+  [qr!^/(essay|client)s/.*\.md(?:text)?!,  set_template_from_capture => {
+    view       => "single_narrative",
+    preprocess => 1,
+    conf       => $conf,
+  }],
+  [qr/\.md(?:text)?/,  single_narrative => {
+    template   => "main.html",
+    preprocess => 1,
+    conf       => $conf,
+  }],
 );
 
 our %dependencies;
 
 if (our $use_dependency_cache and -f "$ENV{TARGET_BASE}/.deps") {
-    open my $deps, "<", "$ENV{TARGET_BASE}/.deps" or die "Can't open .deps for reading: $!";
-    *dependencies = Load join "", <$deps>;
+  open my $deps, "<", "$ENV{TARGET_BASE}/.deps" or die "Can't open .deps for reading: $!";
+  *dependencies = Load join "", <$deps>;
 }
 else {
-    walk_content_tree {
-        for my $lang (qw/en es de fr/) {
-            if (/\.md\.$lang$/ or m!/index\.html\.$lang$! or m!/files/|/slides/|/bin/|/lib/!) {
-                push @{$dependencies{"/sitemap.html.$lang"}}, $_;
-            }
-            if (s!/index\.html\.$lang$!!) {
-                $dependencies{"$_/index.html.$lang"} = [
-                    grep s/^content//, (glob("content$_/*.{md.$lang,pl,pm,pptx}"),
-                                            	glob("content$_/*/index.html.$lang"))
-                ];
-                push @{$dependencies{"$_/index.html.$lang"}}, grep -f && s/^content// && !m!/index\.html\.$lang$!,
-                    glob("content$_/*") if m!/files\b!;
-            }
-        }
-    };
-
-    my @essays_glob = glob("content/essays/files/*/*");
+  walk_content_tree {
     for my $lang (qw/en es de fr/) {
-        push @{$dependencies{"/essays/files/index.html.$lang"}}, grep -f && s/^content// && !m!/index\.html\.$lang$!,
-            @essays_glob;
+      if (/\.md\.$lang$/ or m!/index\.html\.$lang$! or m!/files/|/slides/|/bin/|/lib/!) {
+        push @{$dependencies{"/sitemap.html.$lang"}}, $_;
+      }
+      if (s!/index\.html\.$lang$!!) {
+        $dependencies{"$_/index.html.$lang"} = [
+          grep s/^content//, (glob("content$_/*.{md.$lang,pl,pm,pptx}"),
+                              glob("content$_/*/index.html.$lang"))
+          ];
+        push @{$dependencies{"$_/index.html.$lang"}}, grep -f && s/^content// && !m!/index\.html\.$lang$!,
+          glob("content$_/*") if m!/files\b!;
+      }
     }
-    mkpath $ENV{TARGET_BASE};
-    open my $deps, ">", "$ENV{TARGET_BASE}/.deps" or die "Can't open '.deps' for writing: $!";
-    print $deps Dump \%dependencies;
+  };
+
+  my @essays_glob = glob("content/essays/files/*/*");
+  for my $lang (qw/en es de fr/) {
+    push @{$dependencies{"/essays/files/index.html.$lang"}}, grep -f && s/^content// && !m!/index\.html\.$lang$!,
+      @essays_glob;
+  }
+  mkpath $ENV{TARGET_BASE};
+  open my $deps, ">", "$ENV{TARGET_BASE}/.deps" or die "Can't open '.deps' for writing: $!";
+  print $deps Dump \%dependencies;
 }
 
 1;
