75baa16cc53a396081794fc5bd585a91cad639a3
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2022-10-26 22:53:47 +0000

    comments
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@5468 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- lib/path.pm
+++ lib/path.pm
@@ -7,8 +7,14 @@ use File::Path 'mkpath';
 my $conf = Load join "", <DATA>;
 
 # the only job of this __PACKAGE__ is to fill out the @path::patterns and %path::dependendies data structures.
+#
 # entries in @patterns are three-element arrays:
-# [ $pattern, $method_name (from view.pm), \%args (to pass to method) ].
+# [
+#   $pattern,     # first pattern to match the source file's "/content/"-rooted path wins
+#   $method_name, # provided/implemented in view.pm
+#   \%args,       # to be merged with "path" and "lang" args, and passed (by list value) to view's $method_name)
+# ]
+#
 # entries in %dependencies have keys that represent source file names,
 # with each corresponding value as an array of source files that the key's subsequent built artifact depends on
 # we only unravel the %dependencies at incremental build time, not in full site builds.
