35f3e655de02953cadde2ef3447179e7effb60a4
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2022-11-09 18:40:50 +0000

    bug
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@5503 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- lib/path.pm
+++ lib/path.pm
@@ -58,7 +58,7 @@ else {
 
     if (/\.md[^\/]*$/) {
       read_text_file "content$_", \my %d, 0;
-      push @{$dependencies{$_}}, grep s/^content//, map glob("content$_"), ref $d{dependencies} ? @{$d{headers}{dependencies}} : split /,?\s+/, $d{headers}{dependencies} if exists $d{headers}{dependencies};
+      push @{$dependencies{$_}}, grep s/^content//, map glob("content$_"), ref $d{headers}{dependencies} ? @{$d{headers}{dependencies}} : split /,?\s+/, $d{headers}{dependencies} if exists $d{headers}{dependencies};
     }
 
     for my $lang (qw/en es de fr/) {
