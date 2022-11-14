206a620d43c42f3b4413d9a57ee85b658d815eb2
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2022-11-14 02:16:59 +0000

    CMS commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@6074 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- lib/path.pm
+++ lib/path.pm
@@ -97,7 +97,7 @@ keywords: "mod_perl,c,xs,nodejs,editor.md,python,httpd,apache,git,subversion,zfs
 releases:
   cms:
     url: https://github.com/SunStarSys/cms
-    tag: v2.1.0
+    tag: v2.1.1
   pty:
     url: https://github.com/SunStarSys/pty
     tag: v2.1.1
