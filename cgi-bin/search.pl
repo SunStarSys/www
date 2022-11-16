1923676581c95bbfd1bd030c2c63efdc3dc9a2d9
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2022-11-16 18:49:15 +0000

    search-engine
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@6359 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -92,7 +92,7 @@ $parser->($pffxg, $dirname, undef, \ my %matches);
 my @matches;
 while (my ($k, $v) = each %matches) {
   s/\.md(?:text)?/.html/ for my $link = "/$k";
-  read_text_file "$dirname/$k", \ my %data;
+  read_text_file "$dirname/$k", \ my %data, 0;
   push @matches, [$data{mtime}, qq(<a href="$link">$data{headers}{title}</a>), $v];
 }
 
