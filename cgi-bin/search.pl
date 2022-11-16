fd80e302dc475d54754f46de9c60df68fb8b6858
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2022-11-16 03:20:05 +0000

    search-engine
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@6245 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -40,12 +40,12 @@ my $parser = sub :Sealed {
         my ($pre, $m) = ($1, $2 // "");
         s!\x1b\[[\d;]*m!!g, s!\x1b\[[Km]!!g, s!\{[\{%][^[\}%]+[\}%]\}!!g for $pre, $m;
         my @words;
-        $p->parse($pre), $p->flush;
+        $p->parse($pre), $p->eof;
         push @words, split "\s+", shift @text while @text;
         unshift @words , "" until @words > 5;
         $pre = escape_html join " ", @words[-5 .. -1];
         @words = ();
-        $p->parse($m), $p->flush;
+        $p->parse($m), $p->eof;
         push @words, split "\s+", shift @text while @text;
         $m = qq(<span class="text-success">) . escape_html(join " ", @words[0 .. 4]) . q(</span>);
         $p . $m . " ..."
