eb47d5715e053a27ce85d3c866c956b655252249
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2022-11-16 17:12:09 +0000

    search-engine
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@6299 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -37,7 +37,7 @@ my $parser = sub :Sealed {
       my ($file, $line, $match) = ($1, $2, $3);
       s!\x1b\[[\d;]*m!!g, s!\x1b\[[Km]!!g for $file, $line;
       $match =~ s{(.*?)(?:\x1b\[01;31m(.+?)\x1b\[[Km]([\w\s-]*)|$)}{
-        my ($pre, $m, $tail) = ($1, $2 // "", $3 // "");
+        my ($pre, $m, $tail) = ($1, ($2 // ""), $3 // "");
         my ($first, $last) = ("") x 2;
         $last = $1 if $pre =~ /(\s+)$/;
         s!\x1b\[[\d;]*m!!g, s!\x1b\[[Km]!!g, s!\{[\{%][^[\}%]+[\}%]\}!!g for $pre, $m;
