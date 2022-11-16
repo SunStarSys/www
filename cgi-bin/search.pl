ebd0e68454ae9b535cb2571f904b24dc8ad7984d
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2022-11-16 16:52:41 +0000

    search-engine
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@6285 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -36,7 +36,7 @@ my $parser = sub :Sealed {
     while ($pffxg =~ m{^([^:]+):([^:]+):(.+)$}mg) {
       my ($file, $line, $match) = ($1, $2, $3);
       s!\x1b\[[\d;]*m!!g, s!\x1b\[[Km]!!g for $file, $line;
-      $match =~ s{(.*?)(?:\x1b\[01;31m(.+?)\x1b\[[Km][\w\s-]*|$)}{
+      $match =~ s{(.*?)(?:[\w\s-]*\x1b\[01;31m(.+?)\x1b\[[Km][\w\s-]*|$)}{
         my ($pre, $m) = ($1, $2 // "");
         my ($first, $last) = ("") x 2;
         $first = $1 if $pre =~ /^(\s+)/;
