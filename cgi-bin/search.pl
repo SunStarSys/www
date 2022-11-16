f12de1ce099d35e8c14a828c428cfe10dd840611
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2022-11-16 17:00:45 +0000

    search-engine
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@6293 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -39,7 +39,7 @@ my $parser = sub :Sealed {
       $match =~ s{(.*?)(?:\x1b\[01;31m(.+?)\x1b\[[Km][\w\s-]*|$)}{
         my ($pre, $m) = ($1, $2 // "");
         my ($first, $last) = ("") x 2;
-        $first = $1 if $pre =~ /^(\s+)/;
+        $last = $1 if $pre =~ /(\s+)$/;
         s!\x1b\[[\d;]*m!!g, s!\x1b\[[Km]!!g, s!\{[\{%][^[\}%]+[\}%]\}!!g for $pre, $m;
         my @words;
         $p->parse($pre), $p->eof;
