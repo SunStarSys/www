ed3825fc682ee8d531374c2391b1e14b5f23c9f5
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2022-11-18 00:10:44 +0000

    ssi
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@6553 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -151,8 +151,8 @@ while (my ($k, $v) = each %matches) {
   my ($title) = $data{headers}{title} // $data{content} =~ m/<h1>(.*?)<\/h1>/;
 
   my $total = sum map $_->{count}, @$v;
-  push @matches, [$data{mtime}, $total, qq(<a href="$link">$title</a>), [map $_->{match}, @$v]];
-#   unless $title_cache{$title}++;
+  push @matches, [$data{mtime}, $total, qq(<a href="$link">$title</a>), [map $_->{match}, @$v]]
+    unless $title_cache{$title}++;
 }
 
 @matches = grep {shift(@$_),shift(@$_)} sort {$b->[1] <=> $a->[1] || $b->[0] <=> $a->[0]} @matches;
