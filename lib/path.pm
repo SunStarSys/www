e716cbeaf92b0319cb779de33daee4b8d5822c12
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2022-11-18 23:33:22 +0000

    permalinks
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@6716 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- lib/path.pm
+++ lib/path.pm
@@ -35,11 +35,12 @@ our @patterns = (
   }],
 
   [qr!^/(essay|client)s/.*\.md(?:text)?!, memoize => {
-    view       => [qw/set_template_from_capture snippet single_narrative/],
-    conf       => $conf,
-    archive_root => "/archives",
+    view          => [qw/set_template_from_capture snippet single_narrative/],
+    conf          => $conf,
+    archive_root  => "/archives",
     category_root => "/categories",
-    markdown    => 1, # search markdown instead of built html
+    markdown      => 1, # search markdown instead of built html
+    permalink     => 1,
   }],
 
   [qr!^/(categories|archives)/.*\.md(?:text)?!, memoize => {
