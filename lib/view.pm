13af8c6dafb4ad44aa10c982b67ee4c24368f035
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2022-11-23 18:21:12 +0000

    Rollback r7196.
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@7198 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- lib/view.pm
+++ lib/view.pm
@@ -10,18 +10,18 @@ use warnings;
 # template set from first capture in regex in @path::patterns
 
 sub set_template_from_capture {
-  my %args = @_;
-  $args{template} = "$1.html";
-  my $view = view->next_view(\%args);
-  return view->can($view)->(%args);
+    my %args = @_;
+    $args{template} = "$1.html";
+    my $view = view->next_view(\%args);
+    return view->can($view)->(%args);
 }
 
 sub set_title_from_capture {
-  my %args = @_;
-  $args{headers}{title} = $args{choices}{$1};
-  delete $args{choices};
-  my $view = view->next_view(\%args);
-  return view->can($view)->(%args);
+    my %args = @_;
+    $args{headers}{title} = $args{choices}{$1};
+    delete $args{choices};
+    my $view = view->next_view(\%args);
+    return view->can($view)->(%args);
 }
 
 1;
