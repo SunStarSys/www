60ab2491d060c9b54b0c64359d0c86ecb50f441f
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2022-12-03 23:18:50 +0000

    unnecessary
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@7604 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -175,7 +175,7 @@ while (my ($k, $v) = each %matches) {
   }
   my $filename = "$dirname$k";
   my $uncompressed;
-  gunzip $filename, \$uncompressed and utf8::decode $uncompressed and $filename = \$uncompressed if $filename =~m#\.gz[^/]*$#;
+  gunzip $filename, \$uncompressed and $filename = \$uncompressed if $filename =~m#\.gz[^/]*$#;
   read_text_file $filename, \ my %data, $markdown ? 0 : undef;
   my ($title) = $data{headers}{title} // $data{content} =~ m/<h1>(.*?)<\/h1>/;
   my $total = sum map $_->{count}, @$v;
