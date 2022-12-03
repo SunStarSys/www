4a799238d38f7c7efeda2e82a68df915c22ca64c
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2022-12-03 22:45:10 +0000

    gunzip
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@7594 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -174,7 +174,7 @@ while (my ($k, $v) = each %matches) {
     index($subr->status, "4") == 0 and next;
   }
   my $filename = "$dirname$k";
-  gunzip $filename, \$filename if $filename =~m#\.gz[^/]*$#;
+  gunzip $filename, \$filename and $filename = \$filename if $filename =~m#\.gz[^/]*$#;
   read_text_file "$dirname$k", \ my %data, $markdown ? 0 : undef;
   my ($title) = $data{headers}{title} // $data{content} =~ m/<h1>(.*?)<\/h1>/;
   my $total = sum map $_->{count}, @$v;
