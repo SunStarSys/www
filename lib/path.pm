2976cb523316999892305c8398c1bf074a8abd0a
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2022-12-04 19:41:31 +0000

    reflow
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@7707 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- lib/path.pm
+++ lib/path.pm
@@ -24,8 +24,6 @@ my $conf = Load join "", <DATA>;
 # 2. via a "dependencies" header entry in an md.* file (through seed_deps())
 # 3. via the "dependencies" YAML hash at the bottom of the __DATA__ block below
 
-our $compress = 1;
-
 our @patterns = (
 
 # the "memoize" view corrects most of the speed problems with quick_deps == 3:
@@ -61,6 +59,9 @@ our @patterns = (
   }],
 
 );
+
+# compress copies of text files that don't match a configured regex in @patterns
+our $compress = 1;
 
 #snippet
 our (%dependencies, @acl);
