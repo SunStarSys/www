c89776a91baf6ce3950db9122331f02f276b9104
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2023-01-20 18:04:20 +0000

    diff
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@11421 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -196,7 +196,7 @@ if ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
       my ($revision) = $re =~ /(\d+)$/;
       $diff = $svn->diff($dirname, 1, $revision) if $revision;
       my ($path) = $diff =~ /^Index: (.+)$/m;
-      $svn->info("$dirname$path", sub {$author = $_[1]->last_changed_author});
+      $svn->info("$dirname$path", sub {warn "GOT HERE:$path";$author = $_[1]->last_changed_author});
       ($log) = grep utf8::decode($_), values %{$svn->propget("svn:log", "$dirname$path", $revision, 0)};
     }
     else {
