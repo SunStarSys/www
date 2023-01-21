3c176b7a5a45928173a0e2c36e6e6584ffe0437f
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2023-01-21 01:35:03 +0000

    diff
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@11579 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -199,7 +199,7 @@ if ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
         my $path = "$dirname$1";
         $svn->info($path, sub {$author = $_[1]->last_changed_author; $date = $_[1]->last_changed_date / 1000000});
         ($log) = grep utf8::decode($_), $svn->revprop_get("svn:log", $path, $revision);
-        $date = strftime "%Y-%m-%d %H:%M:s %z (%a, %d %b %Y)", localtime $date;
+        $date = strftime "%Y-%m-%d %H:%M:%S %z (%a, %d %b %Y)", localtime $date;
       }
     }
     else {
