e02e8abd167468225a2191294eb658102a618ccc
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2023-01-21 16:33:09 +0000

    diff
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@11642 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -195,11 +195,12 @@ if ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
     elsif ($re =~ /^diff=/i) {
       my ($revision) = $re =~ /(\d+)$/;
       $diff = $svn->diff($dirname, 1, $revision) if $revision;
-      if ($diff =~ /^Index: (.+)$/m) {
+      while ($diff =~ /^Index: (.+)$/mg) {
         my $path = "$dirname$1";
         eval {$svn->info($path, sub {$author = $_[1]->last_changed_author; $date = $_[1]->last_changed_date / 1000000})};
         ($log) = grep utf8::decode($_), $svn->revprop_get("svn:log", $path, $revision) unless $@;
         $date = strftime "%Y-%m-%d %H:%M:%S %z (%a, %d %b %Y)", localtime $date unless $@;
+        last unless $@;
       }
     }
     elsif ($re =~ /^log=/i) {
