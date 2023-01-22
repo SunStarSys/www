ddf78cacf13203e877b8882946d9a8c79e095733
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2023-01-22 23:40:32 +0000

    fiil
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@11778 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -218,9 +218,9 @@ if ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
         eval {$svn->info($path, sub {$author = $_[1]->last_changed_author; $date = $_[1]->last_changed_date})};
         next if $@;
         ($log) = grep utf8::decode($_), eval{$svn->revprop_get("svn:log", $path, $revision)};
-        my $loc = setlocale(LC_TIME, "$LANG{$lang}.UTF-8");
+        my $loc = setlocale LC_CTYPE, "$LANG{$lang}.UTF-8";
         $date = strftime "%Y-%m-%d %H:%M:%S %z (%a, %d %b %Y)", localtime $date / 1000000 ;
-        setlocale(LC_TIME, $loc);
+        setlocale LC_CTYPE, "$LANG{.en}.UTF-8";
         last unless $@;
       }
     }
@@ -228,9 +228,9 @@ if ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
       my ($revision) = $re =~ /(\d+)$/;
       $log = $svn->log($dirname, "HEAD", $revision);
       for (@$log) {
-        my $loc = setlocale LC_TIME, "$LANG{$lang}.UTF-8";
+        my $loc = setlocale LC_CTYPE, "$LANG{$lang}.UTF-8";
         my $date = strftime "%Y-%m-%d %H:%M:%S %z (%a, %d %b %Y)", localtime $$_[4] / 1000000;
-        setlocale LC_TIME, $loc;
+        setlocale LC_CTYPE, "$LANG{.en}.UTF-8";
         splice @$_, 3, $#$_, "\$Author: $$_[3] \$ \$Date: $date \$";
       }
     }
