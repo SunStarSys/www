f963e05f7ddf300eee374a7a912b58a73317d862
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2023-01-24 02:05:40 +0000

    filter
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@11859 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -350,7 +350,7 @@ if ($re !~ /friends=|watch=|notify=|build=|diff=|log=|acl=|deps=|svnauthz=/i) {
   else {
     my $grep = $unzip[1] == "--markdown" ? "grep" : "xzgrep";
     my @files = map $grep eq "grep" ? $_ : "$_.gz", $apreq->body("files");
-    $pffxg = run_shell_command "cd $d && timeout 30 $grep" => [qw/--color=always --with-filename --line-number --ignore-case -P -e/], $re, @files;
+    $pffxg = run_shell_command "cd $d && timeout 30 $grep" => [qw/--color=always --with-filename --line-number --ignore-case -P -e/], $filter, @files;
   }
 
   if ($?) {
