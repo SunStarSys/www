fb861360b81649a05063475e124e703c3ac54e5e
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2024-02-06 00:08:28 +0000

    text
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@15997 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -470,7 +470,7 @@ if ($re !~ $specials_re) {
     }
     $status =~ s/[^A-Z]//g;
     my $total = sum map $_->{count}, @$v;
-    my $words = join ',', map {encode join '', @$_} map @{$_->{words}}, @$v;
+    my $words = join ',', grep defined && length, map {encode join '', @$_} map @{$_->{words}}, @$v;
     #$words =~ s/[+]+/%20/g;
     #$words =~ s/,%20,/,/g;
     push @matches, [$data{mtime}, $total, qq([<a href="./?regex=^Status:\\s$status;lang=$lang;markdown_search=1"><span class="text-warning">$status</span></a>] <a href="$link#:~:text=$words">$title</a> $rev), $k, [map $_->{match}, @$v]]
