bc28d8a16580537ca47e14f93764c2ec878f4bf4
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2024-02-06 13:42:13 +0000

    quote-embeds
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@16246 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -478,9 +478,8 @@ if ($re !~ $specials_re) {
     my $words = join '&amp;text=', map { my @rv; for my $idx (0..$#{$$_{words}}) { next unless  $$_{words}[$idx] =~ /\S/; push @rv, map "$$_[0]-,$$_[1],-$$_[2]", [map {s/-/%2D/g; s/^\.{3} | \.{3}$//g; $_} encode($$_{pre}[$idx] !~ / \.{3}$/ ? $$_{pre}[$idx] : ""), encode($$_{words}[$idx]), encode($$_{pre}[$idx+1]  !~ /^\.{3} / ? $$_{pre}[$idx+1] : "")]; } @rv } @$v;
     $words =~ s/[+]+/%20/g;
     $words =~ s/%20(&amp;|$)/$1/g;
-    my $i;
     my @w = "text=$words" =~ /text=[^&]+/g;
-    push @matches, [$data{mtime}, $total, qq([<a href="./?regex=^Status:\\s$status;lang=$lang;markdown_search=1"><span class="text-warning">$status</span></a>] <a href="$link#:~:text=$words">$title</a> $rev), $k,  [map {s/(<span class="text-danger">[^<]+<\/span>)/qq(<a href="$link#:~:) . $w[$i++] . qq(">$1<\/a>)/ge; $_} map $_->{match}, @$v]]
+    push @matches, [$data{mtime}, $total, qq([<a href="./?regex=^Status:\\s$status;lang=$lang;markdown_search=1"><span class="text-warning">$status</span></a>] <a href="$link#:~:text=$words">$title</a> $rev), $k, [map {s/(<span class="text-danger">[^<]+<\/span>)/qq(<a href="$link#:~:) . shift(@w) . qq(">$1<\/a>)/ge; $_} map $_->{match}, @$v]]
       unless $title_cache{$title}++;
 
     push @keywords, grep !$keyword_cache{$_}++,  @{ref $data{headers}{keywords} ? $data{headers}{keywords} : [split/[;,]\s*/, $data{headers}{keywords} // ($data{content} =~ m/name="keywords" content="([^"]+)"/i)[0] // ""]};
