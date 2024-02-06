909052bc74b5c9da5fe8d751d750c88f8542433e
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2024-02-06 11:37:06 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@16205 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -82,7 +82,7 @@ sub parser :Sealed {
         my @words;
         $p->parse($pre), $p->eof;
         push @words, split /\s+/, shift @text while @text;
-        if ($m) {
+        if (length $m) {
           if (@words < 5) {
             unshift @words , "" until @words > 5;
           }
@@ -101,7 +101,7 @@ sub parser :Sealed {
         my $extra = @words > 5 ? "..." : undef;
         $m = qq(<span class="text-danger">) . join(" ", grep {defined} @words[0 .. 4], $extra) . q(</span>);
         utf8::decode($_) for @words;
-        push @w, join ' ', @words if length $m;
+        push @w, length($m) ? (join ' ', @words) : undef; 
         push @p, $pre;
         utf8::decode $p[-1];
         $pre . $last . $m
@@ -476,7 +476,7 @@ if ($re !~ $specials_re) {
     }
     $status =~ s/[^A-Z]//g;
     my $total = sum map $_->{count}, @$v;
-    my $words = join '&amp;text=', map { my @rv; for my $idx (0..$#{$$_[0]}) { push @rv, map "$$_[0]-,$$_[1],-$$_[2]", [map {s/-/%2D/g; s/^\.{3} | \.{3}$//g; $_} encode($$_[0][$idx]), encode($$_[1][$idx]), encode($idx > 0 ? $$_[0][$idx-1] : "")]; } @rv } map [$_->{pre}, $_->{words}], @$v;
+    my $words = join '&amp;text=', map { my @rv; for my $idx (0..$#{$$_[0]}) { next unless defined $$_[1][$idx]; push @rv, map "$$_[0]-,$$_[1],-$$_[2]", [map {s/-/%2D/g; s/^\.{3} | \.{3}$//g; $_} encode($$_[0][$idx]), encode($$_[1][$idx]), encode($idx > 0 ? $$_[0][$idx-1] : "")]; } @rv } map [$_->{pre}, $_->{words}], @$v;
     $words =~ s/[+]+/%20/g;
     $words =~ s/%20(&amp;|$)/$1/g;
     $words =~ s/text=[^,]*,%20(?:&amp;|$)//g;
