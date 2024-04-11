5967b5d21749cab70498476861a61aec8b6f0357
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2024-04-11 22:12:43 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@22087 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/enquiry.pl
+++ cgi-bin/enquiry.pl
@@ -17,6 +17,7 @@ my Apache2::RequestRec $r = shift;
 my $DOMAIN = q/sunstarsys.com/;
 my $to     = q/sales@sunstarsys.com/;
 my $date   = gmtime;
+my ($host) = map /^([\w.-]+)$/, $r->headers_in("Host");
 
 sub render :Sealed {
     my Apache2::RequestRec $r = shift;
@@ -25,7 +26,8 @@ sub render :Sealed {
     my APR::Request $apreq = $apreq_class->handle($r);
     my $params = $apreq->param // {};
     my %args = (%$params, @_);
-    local our @TEMPLATE_DIRS = qw(/x1/cms/wcbuild/public/www.sunstarsys.com/trunk/templates);
+    local @TEMPLATE_DIRS = map /(.*)/, </x1/cms/wcbuild/*/$host/trunk/templates>,
+	    "/x1/cms/wcbuild/public/www.sunstarsys.com/trunk/templates";
     $r->content_type("text/html; charset='utf-8'");
     my Dotiac::DTL::Template $dtl = Template($template);
     $r->print($dtl->render(\%args));
