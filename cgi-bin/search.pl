462f76e179576bfdb6e802dc1258ae6260d156c2
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2024-04-22 17:19:05 +0000

    api-err
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@23276 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -437,11 +437,11 @@ if ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
       my $tokens = join '|', ("@"."\Q$svnuser=\E", map "@@"."\Q$_=\E", split ',', (split /:/, $pw{$svnuser})[1]);
       ($revision) = $re =~ /(\d+)$/;
       $revision++ if defined $revision;
-      $log = $svn->log($dirname, $revision);
+      $log = $svn->log($dirname, "HEAD", $revision);
       my ($base, $prefix) = $dirname =~ m!^(.*?)(/content.*)/$!;
       @$log = grep { my $rv; $rv = /^[+][^\n]*(?:$tokens)/ms && !/^[-][^\n]*(?:$tokens)/ms for $svn->diff($dirname, 1, $$_[0]);
                      $rv || scalar grep {s/^.*?\Q$prefix//; my $k=$_; exists $file_seen{$k} || scalar grep index($k, $_) == 0, keys %dir_seen} keys %{$$_[1]}
-                   } grep $svnuser ne $$_[3], @$log;
+                   } grep {$svnuser ne $$_[3] or 1} @$log;
       for (@$log) {
         setlocale LC_TIME, "$LANG{$lang}.UTF-8";
         my @d_fmt = split /\D/, $$_[4];
@@ -451,6 +451,16 @@ if ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
         my ($date) = grep utf8::decode($_), strftime '%Y-%m-%d %H:%M:%S %z (%a, %d %b %Y)', localtime timegm reverse @d_fmt[0..5];
         setlocale LC_TIME, "$LANG{'.en'}.UTF-8";
         splice @$_, 3, $#$_, "\$Author: $$_[3] \$ \$Date: $date \$";
+      }
+      if (@$log) {
+        my $revision = $$log[0][0];
+        my $cookie = APR::Request::Cookie->new(
+          $r->pool,
+          name => "last",
+          value => $revision,
+          expires => "365d",
+        };
+        $r->err_headers_out->set("Set-Cookie" => $cookie->as_string);
       }
     }
   }
