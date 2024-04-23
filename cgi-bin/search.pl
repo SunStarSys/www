e25a2fe7e0a3004731e565853c20818ec53cbf9a
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2024-04-23 01:58:58 +0000

    wcache
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@23495 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -414,8 +414,7 @@ if ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
       my $url;
       $svn->info(substr($dirname, 0 , -1), sub {$url = $_[1]->URL});
       s/:4433//, s/-internal// for $url;
-      my $watchers = do {$wcache{$url} //= $svn->propget("orion:watchers", $url, "HEAD", 1); my %w = %{$wcache{$url}}; \%w};
-      $_ = {map {$_=>1} split /[, ]+/} for values %$watchers;
+      my $watchers = $wcache{$url} //= do { my $w = $svn->propget("orion:watchers", $url, "HEAD", 1); $_ = {map {$_=>1} split /[, ]+/ for values %$w; $w};
       my ($base, $prefix) = $dirname =~ m!^(.*?)(/content.*)/$!;
       while (my ($k, $v) = each %$watchers) {
         $k =~ s/^.*?\Q$prefix//;
