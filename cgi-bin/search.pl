81e403daad4c9305e7cfb1ae2c93d5b3a1aa82c1
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2024-04-24 01:40:43 +0000

    or-cache
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@23818 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -419,7 +419,7 @@ if ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
       my ($base, $prefix) = $dirname =~ m!^(.*?)(/content.*)/$!;
       $svn->info(substr($dirname, 0 , -1), sub {$url = $_[1]->URL});
       s/:4433//, s/-internal// for $url;
-      my ($watchers) = thaw($wcache{"$svnuser-$url"} //= do {
+      my ($watchers) = thaw($wcache{"$svnuser-$url"} ||= do {
         my $w = $svn->propget("orion:watchers", $url, "HEAD", 1);
         $_ = {map {$_=>1} split /[, ]+/} for values %$w;
         while (my ($k, $v) = each %$w) {
@@ -460,7 +460,7 @@ if ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
       $dirname =~ /^(.*)$/ or die "Can't detaint '$dirname'!";
       $dirname = $1;
 
-      ($log) = thaw($ncache{"$svnuser-$dirname-$revision"} //= do {
+      ($log) = thaw($ncache{"$svnuser-$dirname-$revision"} ||= do {
         my $limit;
         $limit = 10 unless defined $revision;
         my $log = $svn->log($dirname, HEAD => $revision, $limit);
@@ -536,7 +536,7 @@ if ($re !~ $specials_re) {
 
   while (my ($k, $v) = each %matches) {
     my $link = $path_info . $k;
-	my $path = "/cms-sites/$host/trunk/content$link";
+    my $path = "/cms-sites/$host/trunk/content$link";
     if ($markdown) {
       $link =~ s/\.md(?:text)?/.html/ or $link =~ s/\.ya?ml\b/.json/;
       eval {
