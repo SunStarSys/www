22724f6f3915d46e87165325deff40cabef28c20
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2024-04-24 22:38:38 +0000

    generify
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@23861 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/enquiry.pl
+++ cgi-bin/enquiry.pl
@@ -5,83 +5,87 @@ use Apache2::RequestUtil;
 use Apache2::RequestIO;
 use APR::Request::Apache2;
 use APR::Request::Param;
+use APR::Request::Cookie;
 use Dotiac::DTL qw/Template *TEMPLATE_DIRS/;
 use Dotiac::DTL::Addon::markup;
 use strict;
 use warnings;
 use base 'sealed';
-use sealed 'daparse';
+use sealed 'deparse';
 
 my Apache2::RequestRec $r = shift;
 
 my $DOMAIN = q/sunstarsys.com/;
-my $to     = q/sales@sunstarsys.com/;
 my $date   = gmtime;
 my ($host) = map /^([\w.-]+)$/, $r->headers_in->get("Host");
 
+my $to = $r->dir_config->get("to") // q/sales@sunstarsys.com/;
+my $validator = $r->dir_config->get("subject_validator") // "orion";
+
 sub render :Sealed {
-    my Apache2::RequestRec $r = shift;
-    my $template = shift;
-    my APR::Request::Apache2 $apreq_class = "APR::Request::Apache2";
-    my APR::Request $apreq = $apreq_class->handle($r);
-    my $params = $apreq->param // {};
-    my %args = (%$params, @_);
-    local @TEMPLATE_DIRS = map /^(.*)$/, </x1/cms/wcbuild/*/$host/trunk/templates>,
-      "/x1/cms/wcbuild/public/www.sunstarsys.com/trunk/templates";
-    $r->content_type("text/html; charset='utf-8'");
-    my Dotiac::DTL::Template $dtl = Template($template);
-    $r->print($dtl->render(\%args));
-    exit 0;
+  my Apache2::RequestRec $r = shift;
+  my $template = shift;
+  my APR::Request::Apache2 $apreq_class;
+  my APR::Request $apreq = $apreq_class->handle($r);
+  my $params = $apreq->param // {};
+  my %args = (%$params, @_);
+  local @TEMPLATE_DIRS = map /^(.*)$/, </x1/cms/wcbuild/*/$host/trunk/templates>,
+    "/x1/cms/wcbuild/public/www.sunstarsys.com/trunk/templates";
+  $r->content_type("text/html; charset='utf-8'");
+  my Dotiac::DTL::Template $dtl = Template($template);
+  $r->print($dtl->render(\%args));
+  exit 0;
 }
 
 if ($r->method eq "POST") {
-    my APR::Request::Apache2 $apreq_class = "APR::Request::Apache2";
-    my APR::Request $apreq = $apreq_class->handle($r);
-    my APR::Request::Param::Table $body = $apreq->body;
-    my ($name, $email, $subject, $content, $site, $username, $confluence, $plan) = @{$body}{qw/name email subject content site username confluence plan/};
-    $confluence = $confluence ? "yes" : "no";
-	defined $content or return;
-    s/\r//g for $name, $email, $subject, $content, $site, $confluence, $plan, $username;
-    s/\n//g for $name, $email, $subject, $confluence, $site, $plan, $username;
+  my APR::Request::Apache2 $apreq_class;
+  my APR::Request $apreq = $apreq_class->handle($r);
+  my APR::Request::Param::Table $body = $apreq->body;
+  my APR::Request::Cookie::Table $jar = $apreq->jar;
 
-    my ($cn, $srs_sender) = ($name, $email);
+  my %vars = @$body;
+  my $content = delete $vars{content};
+  defined $content or return;
+  s/[\r\n]//g for values %vars;
 
-    for ($cn, $subject) {
-        if (s/([^^A-Za-z0-9\-_.,!~*' ])/sprintf "=%02X", ord $1/ge) {
-            tr/ /_/;
-            $_ = "=?utf-8?Q?$_?=";
-        }
+  my ($cn, $srs_sender) = @vars{qw/name email/};
+
+  for ($cn, $vars{subject}) {
+    if (s/([^^A-Za-z0-9\-_.,!~*' ])/sprintf "=%02X", ord $1/ge) {
+      tr/ /_/;
+      $_ = "=?utf-8?Q?$_?=";
     }
+  }
 
-    if ($subject =~ /^orion/i) {
-        s/^(.*)\@(.*)$/SRS0=999=99=$2=$1/, y/A-Za-z0-9._=-//dc for $srs_sender;
-        $srs_sender =~ /(.*)/;
-        length $1 or die "BAD EMAIL: $email";
-        %ENV = ();
+  if ($vars{subject} =~ /$validator/i and $jar->get("nonce")) {
+    s/^(.*)\@(.*)$/SRS0=999=99=$2=$1/, y/A-Za-z0-9._=-//dc for $srs_sender;
+    $srs_sender =~ /^(.*)$/ and length $1 or die "BAD EMAIL: $vars{email}";
+    %ENV = ();
 
-        open my $sendmail, "|-", "/usr/sbin/sendmail", qw/-t -oi -f/, "$1\@$DOMAIN";
-        print $sendmail <<EOT;
+    open my $sendmail, "|-", "/usr/sbin/sendmail", qw/-t -oi -f/, "$1\@$DOMAIN";
+    my $msg <<EOT;
 To: $to
 From: $cn <$srs_sender\@$DOMAIN>
-Reply-To: $cn <$email>
-Subject: $subject
+Reply-To: $cn <$vars{email}>
+Subject: $vars{subject}
 Date: $date +0000
 Content-Type: text/plain; charset="utf-8"
 
-$content
+$vars{content}
 
-WEBSITE: $site
-USERNAME: $username
-CONFLUENCE: $confluence
-Plan: $plan
+---
+
 EOT
-
-        close $sendmail or die "sendmail failed: " . ($! || $? >> 8) . "\n";
+    while ((my $k, $v) = each %vars) {
+      $msg .= "$k: $v\n"
     }
+    print $senmail $msg;
+    close $sendmail or die "sendmail failed: " . ($! || $? >> 8) . "\n";
+  }
 
-    render $r, "enquiry_post.html",
-        content => "## Thank You!\n\nOur Sales Team will get back to you shortly.\n",
-        headers => { title => "Sales Enquiry" };
+  render $r, "enquiry_post.html",
+    content => "## Thank You!\n\nOur Sales Team will get back to you shortly.\n",
+    headers => { title => "Sales Enquiry" };
 }
 
 render $r, "enquiry_get.html";
