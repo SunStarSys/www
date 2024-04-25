11fe4caabb7e5c96c7a4b16683f13327387e01a1
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2024-04-25 23:43:01 +0000

    futzing
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@24128 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -461,7 +461,7 @@ if ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
         my $limit;
         $limit = 10 unless defined $revision;
         my $log = $svn->log($dirname, HEAD => $revision, $limit);
-        push @$_, $svn->diff($dirname, 1, $$_[0]) for @$log;
+        push @$_, map {utf8::encode $_; $_} $svn->diff($dirname, 1, $$_[0]) for @$log;
         @$log = grep $$_[3] ne $svnuser, @$log if IGNORE_SELFIES;
         freeze {log => $log, time => $r->request_time}
       });
