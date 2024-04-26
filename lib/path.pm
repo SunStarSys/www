44f8e68652a53230d115b9ba38a881226677e24b
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2024-04-26 23:49:45 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@24343 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- lib/path.pm
+++ lib/path.pm
@@ -45,7 +45,7 @@ our @patterns = (
   [qr!\.ya?ml\b[^/]*$!, yml2ext => { compress => 1 }],
 
   [qr!/(api|clients)/.*\.md(?:text)?!, memoize => {
-    view            => [qw/set_template_from_capture snippet single_narrative/],
+    view            => [qw/set_template_from_capture snippet langify_template  single_narrative/],
     compress        => 1,
     facts           => $facts,
     markdown_search => 1, # search markdown instead of built html
@@ -59,7 +59,7 @@ our @patterns = (
   }],
 
   [qr#^(?!/editor\.md/).*\.md(?:text)?[^/]*$#, memoize => {
-    view       => [qw/snippet asymptote single_narrative/],
+    view       => [qw/snippet asymptote langify_template  single_narrative/],
     compress   => 1,
     template   => "main.html",
     facts      => $facts,
@@ -80,7 +80,7 @@ walk_content_tree {
 
   seed_file_deps, seed_file_acl if /\.(?:md|ya?ml)[^\/]*$/;
 
-  for my $lang (qw/en es de fr/) {
+  for my $lang (qw/en es de ru sv he zh-TW fr/) {
 
     if (/\.md\.$lang$/ or m!/index\.html\.$lang$! or m!/files/|/slides/|/bin/!) {
       push @{$dependencies{"/sitemap.html.$lang"}}, $_ if !archived;
