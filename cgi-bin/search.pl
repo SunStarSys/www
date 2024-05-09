6e9bbf4f6b0d29ff4030903221dc81fb1401913a
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2024-05-09 18:37:22 +0000

    NO_MMAP
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@24943 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -233,7 +233,7 @@ my $host     = $r->headers_in->{host};
 my ($js, $count);
 
 
-my $env = BerkeleyDB::Env->new(-Home => "/x1/tmp", -Flags => DB_CREATE|DB_INIT_CDB|DB_INIT_MPOOL|DB_THREAD, -ErrFile => *STDERR) or die "Can't create DB env: $!";
+my $env = BerkeleyDB::Env->new(-Home => "/x1/tmp", -Flags => DB_CREATE|DB_INIT_CDB|DB_INIT_MPOOL|DB_THREAD|DB_NOMMAP, -ErrFile => *STDERR) or die "Can't create DB env: $!";
 my $dbn = tie my %ncache, 'BerkeleyDB::Hash', -Filename => "ncache", -Flags => DB_CREATE, -Env => $env or die "Can't open ncache DB: $!";
 my $dbw = tie my %wcache, 'BerkeleyDB::Hash', -Filename => "wcache", -Flags => DB_CREATE, -Env => $env or die "Can't open wcache DB: $!";
 
