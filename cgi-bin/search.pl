127065dcd5b61337b72b4b0f07de26de1c9b3168
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2024-05-09 16:29:47 +0000

    BerkeleyDB
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@24922 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -232,8 +232,10 @@ my $hash     = $apreq->body("hash") // "";
 my $host     = $r->headers_in->{host};
 my ($js, $count);
 
-tie my %ncache, 'BerkeleyDB::Hash', -Filename => "/x1/tmp/ncache", -Flags => DB_CREATE|DB_INIT_CDB|DB_INIT_MPOOL or die "Can't open ncache DB: $!";
-tie my %wcache, 'BerkeleyDB::Hash', -Filename => "/x1/tmp/wcache", -Flags => DB_CREATE|DB_INIT_CDB|DB_INIT_MPOOL or die "Can't open wcache DB: $!";
+
+my $env = BerkeleyDB::Env->new(-Home => "/x1/tmp", -Flags => DB_CREATE|DB_INIT_CDB|DB_INIT_MPOOL or die "Can't create DB env: $!";
+tie my %ncache, 'BerkeleyDB::Hash', -Filename => "ncache", -Flags => DB_CREATE, -Env => $env or die "Can't open ncache DB: $!";
+tie my %wcache, 'BerkeleyDB::Hash', -Filename => "wcache", -Flags => DB_CREATE, -Env => $env or die "Can't open wcache DB: $!";
 
 utf8::decode($_) for $re, $filter;
 
