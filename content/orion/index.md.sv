b7a07b20710878c3f74a97ee7430e6c5bdb91aea
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-02-17 18:47:27 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@27758 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- content/orion/index.md.sv
+++ content/orion/index.md.sv
@@ -104,7 +104,7 @@ Vårt globala POP-nätverk (Point of Presence) garanterar anslutningar med kort 
 
 <h3>Det är dags för en havsförändring i hur du hanterar och levererar din webbplats till din målgrupp.</h3>
 
-Gå i din egen takt, använd så många anställda / förhandsvisningar / redigeringar / åtaganden / grenar som du vill, och betala bara för vad du * faktiskt använder * på vår infrastruktur. Ta bara med ditt eget AI-förbättrade css/javascript-ramverk, perl-skript och Django-mallar, så tar vi hand om resten av din installation utan extra kostnad. Vi rekommenderar **Bootstrap ** för css / js styling, eftersom det är vad vi använder internt; men det är helt upp till dig.
+Gå i din egen takt, använd så många anställda / förhandsvisningar / redigeringar / åtaganden / grenar som du vill, och betala bara för vad du * faktiskt använder * på vår infrastruktur. Ta bara med ditt eget AI-förbättrade css/javascript-ramverk, perl-skript och Django-mallar, så tar vi hand om resten av din installation utan extra kostnad. Vi rekommenderar **Bootstrap 5** för css/js styling, eftersom det är vad vi använder internt; men det är helt upp till dig.
 
 &nbsp;
 
