2a26916380e69e32f4bb511655b4bb67f10cd0bf
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-02-17 19:27:11 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@27768 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- content/orion/index.md.sv
+++ content/orion/index.md.sv
@@ -106,6 +106,8 @@ Vårt globala POP-nätverk (Point of Presence) garanterar anslutningar med kort 
 
 Gå i din egen takt, använd så många anställda / förhandsvisningar / redigeringar / åtaganden / grenar som du vill, och betala bara för vad du * faktiskt använder * på vår infrastruktur. Ta bara med ditt eget AI-förbättrade css/javascript-ramverk, perl-skript och Django-mallar, så tar vi hand om resten av din installation utan extra kostnad. Vi rekommenderar **Bootstrap 5** för css/js styling, eftersom det är vad vi använder internt; men det är helt upp till dig.
 
+Dessutom har alla kunder gratis, mallanpassningsbara eller javascript-aktiverade gränssnitt till vår kraftfulla, egenutvecklade PCRE-sökmotor.
+
 &nbsp;
 
 Se våra [Orionplaner](plans) för de aktuella avtalen.
