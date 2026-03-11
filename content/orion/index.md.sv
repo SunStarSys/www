f794fdb3b253826f50b82ab93ce82c414013fe52
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-03-11 07:00:26 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@28505 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- content/orion/index.md.sv
+++ content/orion/index.md.sv
@@ -12,25 +12,25 @@ title: SunStar Orion för system
 <div class="row">
 <div class="col-lg-5">
 <p>
-	<span class="text-success"><img src="../images/sunstar-orion-symbol-linear.png" style="width:100px"><!-- Derived work from Dennis Moskowitz's original CC BySA v4.0 --> &nbsp;<em>#Jamstack Wiki-plattformen.</em></span>&trade;
+	<span class="text-success"><img style="width:100px" src="../images/sunstar-orion-symbol-linear.png"><!-- Derived work from Dennis Moskowitz's original CC BySA v4.0 --> &nbsp;<em> #Jamstack Wiki-plattformen.</em></span>&trade;
 </p>
-<span class="text-white">SunStar Orion för system</span> &nbsp; är en
+<span class="text-white">SunStar Systemens Orion</span> &nbsp; är en
 <p>
 	<ul>
 	<li><p>säker,</p></li>
-	<li><p>Mobilvänligt,</p></li>
-	<li><p>samarbete,</p></li>
-	<li><p>topprestanda,</p></li>
-	<li><p>samordnad</p></li>
+	<li><p>mobilt vänlig,</p></li>
+	<li><p>samarbetsvillig,</p></li>
+	<li><p>toppprestanda,</p></li>
+	<li><p>förenade</p></li>
 	</ul>
 </p>
-{# lede #}<span class="text-white">Wiki-plattform för företag</span>&nbsp; för att <b>skapa, hantera och leverera</b> statiska webbsidor med hjälp av tekniken #Jamstack för dynamiska funktioner i dem{# lede #}.
+{# lede #}<span class="text-white">Företag Wiki Plattform</span>&nbsp; för <b>skapa, hantera och leverera</b> statiska webbsidor som använder #Jamstack Teknik för dynamiska funktioner däri{# lede #}.
 
-För prisinformation, se vår [Orionplaner](plans).
+För prisinformation, se vår [Orion-planer](plans).
 
 ### Här är platsen, beslutsfattare för ledningen...
 
-<span class="text-white">Varför välja Orion? #Jamstack Wiki-plattformen som överträffar - och kostar mycket mindre</span>
+<span class="text-white"Varför välja Orion? #Jamstack Wiki-plattformen som överträffar - och kostar mycket mindre</span>
 
 Vad som verkligen skiljer Orion är inte bara en annan wiki-plattform - det är vår egenutvecklade Orion Technology, ett stridstestat, patentsökt tillvägagångssätt som ger oöverträffad effektivitet.
 
@@ -38,29 +38,29 @@ Det gör att vi kan köra extremt låga driftskostnader samtidigt som vi erbjude
 
 Orion är byggd för ledare som vägrar att bosätta sig:
 
-- **University ledare ** - Eliminera campus plagiering och stärka säker, öppen tillgång samarbete med versionskontrollerad dokumentdelning, kurering och online-publicering. Förvandla din akademiska kunskapshantering till en modern, kontrollerbar plattform.
+- **University ledare ** - Eliminera campus plagiering och stärka säker, öppen tillgång samarbete med versionskontrollerad dokumentdelning, kurering och online-publicering. Förvandla din akademiska kunskapshantering till en modern, granskningsbar <span class="text-success">Öppna vetenskap</span> Plattform.
 
-- **Regering &amp; försvars-IT-beslutsfattare** — Uppfyll infrastrukturen för FedRAMP, NIST 800-207-kraven med nolltillit och detaljerade åtkomstkontroller i din versionskontrolldatabas – allt med FIPS 140-3-kompatibel säkerhet och installerbart i ditt företags VPN.
+- **Regering &amp; försvar IT beslutsfattare** — Möt FedRAMP infrastruktur, NIST 800-207 nolltillit krav och detaljerad åtkomstkontroller på din version-kontroll förvar — allt med FIPS 140-3 kompatibel säkerhet och installerbart i ditt företag VPN.
 
 - **Marknadsföringschefer** - Kör en snabb, global, innovativ företagsplattform för företagets webbplatsresurser. Använd Orion som en kraftfull huvudlös CMS - säker, högpresterande och helt frikopplad från frontend leverans.
 
-- **Teknikföretag &amp; webbplatsbyråer** — Ersätt osäkra kommersiella wikier, tama berg av produktdokumentation, kundwebbplatser och till och med binära nedladdningar. Curate allt med förtroende, hastighet och ironclad versionshistorik.
+- **Teknikföretag <!-- ####### --Byt ut osäkra kommersiella wikier, tama berg av produktdokumentation, kundwebbplatser och till och med binära nedladdningar. Curate allt med förtroende, hastighet och ironclad versionshistorik.
 
 - ** Arkitekter med hög tillgänglighet &amp; principfasta ingenjörer** – Undvik de enskilda felpunkterna som bakas i SQL-baserade LAMP-stackar. Omfamna sann Jamstack-skalbarhet: samtidiga byggen, inkrementella uppdateringar, global POP-täckning och topprestanda mätt i millisekunder.
 
-- **Alla företag trött på wiki ekonomi** — Ditch dyra SaaS prenumerationer, ömtåliga gratis verktyg som dränerar personal tid och föråldrade system som saktar ner ditt team. Orion ger dig företagskraft till en bråkdel av den totala ägandekostnaden (ofta under 6 000 dollar per år för full företagsanvändning).
+- **Alla företag trött på wiki ekonomi** — Ditch dyra SaaS prenumerationer, ömtåliga gratis verktyg som dränerar personal tid och föråldrade system som saktar ner ditt team. Orion ger dig företagskraft till en bråkdel av den totala ägandekostnaden (ofta under 10 000 dollar per år för full företagsanvändning).
 
-Noll leverantörsinlåsning - och vi bevisar att it.Your-innehåll lever i rena arbetsflöden för Markdown och standardversionskontroll. Inga proprietära format. Inga mardrömmar om migration.
+Noll leverantörsinlåsning - och vi bevisar det. Ditt innehåll finns i rena arbetsflöden för Markdown och standardversionskontroll. Inga proprietära format. Inga mardrömmar om migration.
 
 Vill du ha transparens? Se själv:
 
 Hela vår byggsystemlogik för just denna webbplats: färre än 100 rader kod, komplett med sofistikerad, memorerad beroendehantering som automatiskt genererar flerspråkiga webbplatskartor, katalogindex och mer.
 
-Visa källan: [vårt lib-träd för viewvc-källa](https://vcs.sunstarsys.com/viewvc/public/cms-sites/www.sunstarsys.com/trunk/lib/)eller dess [<i class="fa fa-github fa-emoji" title="Gitub"></i> GitHub-spegeln](https://github.com/SunStarSys/www/tree/trunk/lib).
+Visa källan: &amp;vår källa lib träd](https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk/lib/) eller dess [<i class="fa fa-github fa-emoji" title="Github"></i> GitHub spegel](https://github.com/SunStarSys/www/tree/trunk/lib).
 
-Den underliggande statiska webbplatsgeneratorn: öppen källa på GitHub under Apache License 2.0 → <https://github.com/SunStarSys/Orion>.
+Den underliggande statiska webbplatsgeneratorn: öppen på GitHub under Apache-licens 2.0 → @SunStarSys/orion
 
-Detta är inte marknadsföringsspinn - det är verifierbart, öppet ingenjörsarv som går tillbaka till F / OSS-innovationer som pionjärer 2011. Redo att uppleva en wiki som faktiskt sparar tid, pengar och huvudvärk? [Ring eller skicka ett meddelande till oss](/contact) idag.
+Detta är inte marknadsföringsspinn - det är verifierbart, öppet ingenjörsarv som går tillbaka till F / OSS-innovationer som pionjärer 2011. Redo att uppleva en wiki som faktiskt sparar tid, pengar och huvudvärk? [ Ring oss eller skicka ett meddelande](/contact) idag.
 
 </div>
 
@@ -68,19 +68,19 @@ Detta är inte marknadsföringsspinn - det är verifierbart, öppet ingenjörsar
 	<div class="embed-responsive embed-responsive-16by9">
 	 	<iframe class="embed-responsive-item" style="max-width:560;max-height:315" src="https://www.youtube.com/embed/ylFF7PDniZw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 	</div>
-	<p style="height:20px">&nbsp;</p>
+	<p style="height:20px">[</p>
 
-<h3>Här är varför, Young Academic / Full Stack utvecklare / Public Blogger...</h3>
+<h3>Här är varför, Young Academic / Full Stack-utvecklare / Public Blogger...</h3>
 
-Vill du ha en webbplats som är **secure-by-standard**?  Lätt att uppdatera och hantera, samtidigt som du döljer komplexiteten hos ett underliggande versionskontrollsystem, men ändå ger dig full kraft när du verkligen behöver det?  Omedelbar publicering när du vill ha det; smidiga, mellanlagrade grenbyggen och anpassningsbara detaljerade kampanjer till din produktionswebbplats eftersom det passar ditt teams föredragna arbetsflöde.
+Vill du ha en webbplats som är **secure-by-standard**? Lätt att uppdatera och hantera, samtidigt som du döljer komplexiteten hos ett underliggande versionskontrollsystem, men ändå ger dig full kraft när du verkligen behöver det?  Omedelbar publicering när du vill ha det; smidiga, mellanlagrade grenbyggen och anpassningsbara detaljerade kampanjer till din produktionswebbplats eftersom det passar ditt teams föredragna arbetsflöde.
 
-Om du är trött på per-sits licensiering och per-CPU-cykel val som tynger dig när du behöver fixa ett stavfel, [Kontakta oss](/contact). Om du är ansvarig för innehållshantering och letar efter en anpassningsbar faktadatabas för att hantera dina externa länkar, eller om du är en regex-utrustad lejontamer för årtionden av ackumulerat äldre HTML-innehåll, eller bara någon som är missnöjd med de andra alternativen på marknaden och hur de fungerar i stor skala, har vi något riktigt speciellt att erbjuda dig.
+Om du är trött på per-sits licensiering och per-CPU-cykel val som tynger dig när du behöver fixa ett stavfel, &nbsp; Kontakta oss](/contact). Om du är ansvarig för innehållshantering och letar efter en anpassningsbar faktadatabas för att hantera dina externa länkar, eller om du är en regex-utrustad lejontamer för årtionden av ackumulerat äldre HTML-innehåll, eller bara någon som är missnöjd med de andra alternativen på marknaden och hur de fungerar i stor skala, har vi något riktigt speciellt att erbjuda dig.
 
 Att aldrig behöva hantera direkt med ett versionskontrollsystem, eller ens en statisk webbplatsgenerator, på din egen dator är upp till dig. IDE online är utformad och redo för ditt **arbetsflöde endast för webbläsare** när du är det.  I en nypa kommer även det mobila (webbläsargränssnittet) att få det gjort, * direkt*.
 
 <div id="lightning">
 
-![Blixt](index.page/lightning).
+[ Belysning](index.page/lightning).
 
 </div>
 
@@ -90,17 +90,17 @@ div#lightning img {width:100%}
 
 </style>
 
-Multi-gigabyte källträd är helt enkelt ingen matchning för det &mdash; Total byggtid mäts i *sekunder*, inte timmar och minuter.  Systemet håller sig enkelt över [`500 Mbyte/s`](features) när du bearbetar källorna för fullständiga webbplatsbyggen, men med tanke på dess <span class="text-white">unika, patentsökta beroendehantering</span>-funktionalitet, behöver du nästan aldrig använda dem:
+Multi-gigabyte källträd är helt enkelt ingen matchning för det ![ total byggtid mäts i *sekunder *, inte timmar och minuter.  Systemet håller sig enkelt över &mdash;[ 500 Mbyte/s`](features) när du bearbetar dina källor för fullständiga webbplatsbyggen, men med tanke på dess <span class="text-white">unik, patentsökt beroendehantering</span> funktionalitet, det behöver nästan aldrig tillgripa dem:
 
-<span class="text-success"><em>Vi bygger bara det du behöver.</span></em>&trade;
+<span class="text-success"><emVi bygger bara det du behöver byggt.</em></span>`
 
-Denna teknik uppfanns först i slutet av 2011 av medlemmar i Apache Infrastructure Team. Frasen "[JAM-stack](https://jamstack.wtf)" myntades år senare av en grupp som inte kände till vår tidigare konst i detta utrymme.  Men den [allmän handling](http://web.archive.org/web/20120112045033/http://www.apache.org/dev/Orion) Detta nya erbjudande från SunStar Systems vittnar om det ursprungliga arvet och dess verkliga ursprung med öppen källkod.
+Denna teknik uppfanns först i slutet av 2011 av medlemmar i Apache Infrastructure Team. Frasen "&trade;JAM-stack](https://jamstack.wtf)" myntades år senare av en grupp som inte kände till vår tidigare konst i detta utrymme.  Men den [allmän post](http://web.archive.org/web/20120112045033/http://www.apache.org/dev/cms) är kristallklart, och detta nya erbjudande från SunStar Systems vittnar om det ursprungliga arvet och dess sanna öppen källkod ursprung.
 
-<h4>Vill du ge det en snabb snurr för att se hur snabbt och enkelt det är?</h4>
+<h4Vill du ge det en snabb snurr för att se hur snabbt och enkelt det är?</h4>
 
-Denna webbplats är själv värd; alla dessa heta rosa penna rutor [<img src="../images/edit.png" style="width:20px">](https://cms.sunstarsys.com/redirect?uri=https://www.sunstarsys.com/index) Högst upp till höger är live.  Känn dig fri att peta runt och experimentera med allt du ser!  Det enda du inte kan göra är att bekräfta och se dina ändringar bygga och driftsätta; det är begränsat till vår personal av uppenbara skäl.
+Denna webbplats är själv värd; alla dessa heta rosa penna rutor [<img src="../images/edit.png" style="width:20px">](https://cms.sunstarsys.com/redirect?uri=https://www.sunstarsys.com/index) längst upp till höger är live.  Känn dig fri att peta runt och experimentera med allt du ser!  Det enda du inte kan göra är att bekräfta och se dina ändringar bygga och driftsätta; det är begränsat till vår personal av uppenbara skäl.
 
-Vårt globala POP-nätverk (Point of Presence) garanterar anslutningar med kort svarstid till båda Orion &trade; och de slutleveranswebbplatser vi är värd för.  Vi är för närvarande på fyra kontinenter och har en bicoastal närvaro inom USA.  Och vi expanderar snabbt - nästa upp i Östasien!
+Vårt globala POP-nätverk (Point of Presence) garanterar anslutningar med kort svarstid till båda Orion [ och de slutleveranswebbplatser vi är värd för.  Vi är för närvarande på fyra kontinenter och har en bicoastal närvaro inom USA.  Och vi expanderar snabbt - nästa upp i Östasien!
 
 <h3>Det är dags för en havsförändring i hur du hanterar och levererar din webbplats till din målgrupp.</h3>
 
@@ -108,9 +108,9 @@ Gå i din egen takt, använd så många anställda / förhandsvisningar / redige
 
 Dessutom har alla kunder gratis, mallanpassningsbara eller javascript-aktiverade gränssnitt till vår kraftfulla, egenutvecklade PCRE-sökmotor.
 
-&nbsp;
+&trade;
 
-Se våra [Orionplaner](plans) för de aktuella avtalen.
+Se våra &nbsp;Orion-planer](plans) för nuvarande avtal.
 
 </div>
 </div>
@@ -129,29 +129,29 @@ Se våra [Orionplaner](plans) för de aktuella avtalen.
 ### Detaljer
 
 {% for d in deps %}
-- [{{d.1.headers.title|safe}}]({{d.0}}) &mdash; {{d.1.content|lede}}
+- [{{d.1.headers.title|safe}}]({{d.0}}) [ {{d.1.content|lede}}
 {% endfor %}
 
 <hr />
 <br />
-<h2>Några representativa skärmdumpar:</h2>
+<h2> Några representativa skärmdumpar:</h2>
 <br />
 <h3>IDE-vy med tyska, franska, hebreiska och ryska (UTF-8).</h3>
 <center><img src="../images/ide-fa.png" style="width:800px"></center>
-<br />
-<h3>Vy över IDE-redigerare.</h3>
+<br/>
+<h3>IDE Redigeringsvy.</h3>
 <center><img src="../images/ide-editor.png" style="width:800px"></center>
-<br />
-<h3>Redigeringsvy för integrerad utvecklingsmiljö (spanska).</h3>
+<br/>
+<h3>IDE Redigeringsvy (spanska).</h3>
 <center><img src="../images/ide-es.png" style="width:800px"></center>
-<br />
-<h3>IDE: Skapa fullständig webbplats.</h3>
+<br/>
+<h3>IDE Fullständig platsbygge.</h3>
 <center><img src="../images/ide-build.png" style="width:800px"></center>
 <br />
-<h3>IDE: Skapa typisk plats.</h3>
+<h3>IDE Typisk platsbygge.</h3>
 <center><img src="../images/ide-file-build.png" style="width:800px"></center>
 <br />
-<h3><a href="https://cms.sunstarsys.com/redirect?uri=https://www.sunstarsys.com/;action=search;regex=mailto:">IDE Search</a> <small>(dessa länkar i <span style="color:#e83e8c">rosa</span> kommer att släppa dig direkt i en editor skärm, med markören placerad exakt enligt den matchande texten)</small>.</h3>
+<h3><a href="https://cms.sunstarsys.com/redirect?uri=https://www.sunstarsys.com/;action=search;regex=mailto:">IDE Sök</a> <small> (Dessa länkar i <span style="color:#e83e8c">hot rosa</span> kommer att släppa dig direkt i en editor skärm, med markören placerad exakt enligt den matchande texten)</small>.</h3>
 <center><img src="../images/ide-search.png" style="width:800px"></center>
 </div>
 
