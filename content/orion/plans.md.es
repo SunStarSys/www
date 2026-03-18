2f9af862ff5361969c513915ed883b1d259da3f7
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-03-18 20:27:33 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@29150 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- content/orion/plans.md.es
+++ content/orion/plans.md.es
@@ -168,9 +168,9 @@ $1000 / mes, ~67% de descuento en todos los modelos de precios de la competencia
 
 - Descuentos por volumen disponibles.
 
-- Las instalaciones de servidor Orion basadas en Enterprise Kerberos ya están disponibles para los clientes que buscan alternativas a Atlassian Confluence.
+- Las instalaciones de servidor Orion basadas en Enterprise Kerberos ya están disponibles para los clientes que buscan alternativas al Centro de datos Atlassian Confluence.
 
-- Integración de tecnología sin servidor próximamente.
+- OCI Function (Serverless Technology) / OCI RDBMS Integration próximamente.
 
 - Servicios de consultoría y construcción de sitios web de capacitación y transporte de tecnología disponibles.
 
--- content/orion/plans.md.sv
+++ content/orion/plans.md.sv
@@ -168,9 +168,9 @@ $ 1000 / månad, ~ 67 % rabatt på alla konkurrent prismodeller!
 
 - Volymrabatter tillgängliga.
 
-- Företag Kerberos-baserade Orion Server Installs är nu tillgängliga, för kunder som söker alternativ till Atlassian Confluence.
+- Företag Kerberos-baserade Orion Server Installs är nu tillgängliga, för kunder som söker alternativ till Atlassian Confluence Data Center.
 
-- Serverless Technology Integration kommer snart.
+- OCI-funktion (serverlös teknik)/OCI RDBMS-integrering kommer snart.
 
 - Konsulttjänster och tjänster för teknisk utbildning och portering av webbsidor.
 
@@ -232,4 +232,4 @@ $ 1000 / månad, ~ 67 % rabatt på alla konkurrent prismodeller!
 h4 { font-size:14px }
 </style>
 
-<!-- $Date: 2026-03-18 12:24:36 -0700 (Wed, 18 Mar 2026) $ $Author: joe $ $Revision: 29136 $ -->
+<!-- $Date: 2026-03-18 13:27:08 -0700 (Wed, 18 Mar 2026) $ $Author: joe $ $Revision: 29149 $ -->
