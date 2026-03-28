c2737d20ae41c2a34255e876eccc22668b563900
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-03-28 17:43:57 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@30689 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -320,8 +320,8 @@ if ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
         $filter = $1 if $pw{$svnuser} =~ /\bsvnadmin\b/ and $filter =~/(.*)/;
         while (<$fh>) {
           /^$host/i and !/"HEAD / and /"[^" ]+ ([^" ]+) HTTP/ and $1 =~ /^\Q$prefix/ or next;
-          /$filter/i or next if $filter;
-          /$prefilter/i or next if $prefilter;
+          /$filter/i or next if length $filter;
+          /$prefilter/i or next if length $prefilter;
           s{ ([45])(\d\d) }{$1 == 4 ? $e4xx++ : $e5xx++;q/ <span class="text-/ . ($1 == 4 ? q/warning">/ : q/danger">/)."$1$2</span> " }e
             if +(split /\s(?:"[^"]*"\s)*/)[6] >= 400;
           push @weblog, $_;
