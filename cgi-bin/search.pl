c40665de723d1b4e08105d03d0464304f652d128
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-03-28 17:02:09 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@30661 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -312,13 +312,15 @@ if ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
         read $fh, $blog, -s $fh;
       }
     }
-    elsif ($re =~ /^weblog=/i) {
-      if (open my $fh, "<:raw", "/x1/logs/httpd/access_log") {
+    elsif ($re =~ /^weblog=(.*)$/i) {
+	 my $prefilter = $1;
+     if (open my $fh, "<:raw", "/x1/logs/httpd/access_log") {
         my $prefix = $r->path_info;
         $filter = $1 if $pw{$svnuser} =~ /\bsvnadmin\b/ and $filter =~/(.*)/;
         while (<$fh>) {
           /^$host/i and !/"HEAD / and /"[^" ]+ ([^" ]+) HTTP/ and $1 =~ /^\Q$prefix/ or next;
           /$filter/ or next if $filter;
+		  /$prefilter/ or next if $prefilter;
           s{ ([45])(\d\d) }{$1 == 4 ? $e4xx++ : $e5xx++;q/ <span class="text-/ . ($1 == 4 ? q/warning">/ : q/danger">/)."$1$2</span> " }e
             if +(split /\s(?:"[^"]*"\s)*/)[6] >= 400;
           push @weblog, $_;
@@ -342,6 +344,8 @@ if ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
         $stddur /= (@duration || 1);
         $stddur -= $meandur**2;
         $stddur = sqrt($stddur);
+
+        $re =~ s/^weblog=(.*)$/weblog=$filter/ and $filter = "" if $filter;
 
         $tbw = sum @bandwidth;
         $meanbw = $tbw / (@bandwidth || 1);
