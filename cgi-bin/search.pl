1efc546db25f2470d08fbe1e858daa904a51ac9a
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-03-28 21:33:40 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@30802 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -522,7 +522,7 @@ elsif ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
       });
 
       delete $wcache{"$svnuser-$url"} unless $r->request_time - $watchers->{time} < 100_000;
-      $dbw->cds_unlock and undef $lock if $lock;
+      $lock->cds_unlock and undef $lock if $lock;
       $watchers = $watchers->{hash};
 
       while (my ($k, $v) = each %$watchers) {
@@ -555,7 +555,7 @@ elsif ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
       });
 
       delete $ncache{"$svnuser-$dirname-$revision"} unless defined $revision and $r->request_time - $log->{time} < 1000;
-      $dbn->cds_unlock and undef $lock if $lock;
+      $lock->cds_unlock and undef $lock if $lock;
 
       $log = $log->{log};
 
