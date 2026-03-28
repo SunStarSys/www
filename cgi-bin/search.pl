fbdc256bbbb68239e49e48fef723c892ff56f4e0
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-03-28 20:42:17 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@30796 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -684,7 +684,7 @@ my %title = (
   ".zh-TW" => "$markdown 的搜尋結果",
 );
 
-if (@matches) {
+unless (@weblog) {
   $hash = "Digest::SHA1";
   $hash = $hash->new;
   $hash->add(join ":", $r->dir_config("CookieSecret"), map $$_[1], @matches);
