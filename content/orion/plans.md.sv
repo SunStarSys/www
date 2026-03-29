f24496ec702f6eff1d907c4b579510ded33b1dcf
(no author) <(no author)@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-03-29 11:18:37 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@30827 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- content/orion/plans.md.sv
+++ content/orion/plans.md.sv
@@ -49,7 +49,7 @@ $25/månad/hanterad allmän webbplats
 						<li>Dataöverföring på 10 USD/40 Gbyte</li>
 						<li>$5/säte</li>
 						<li>$10/100 byggminuter</li>
-                        <li>$0,10 / sida AI NLP översättningar</li>
+                        <li>$0,50 / sida AI NLP översättningar</li>
 </ol>
 					</p>
 </li>
@@ -95,7 +95,7 @@ $ 100 / månad / hanterad offentlig webbplats, ~ 50 % rabatt på alla konkurrent
 						<li>Dataöverföring på 20 USD/100 Gbyte</li>
 						<li>$10/säte</li>
 						<li>$10/100 byggminuter</li>
-                        <li>$0,06 / sida AI NLP översättningar</li>
+                        <li>$.40 / sida AI NLP översättningar</li>
                     </ol>
 					</p>
 					</li>
@@ -149,7 +149,7 @@ $ 1000 / månad, ~ 67 % rabatt på alla konkurrent prismodeller!
 						<li>$100 / ithread mod_perl registervärd</li>
 						<li>$100/OCI-region för anpassade POP</li>
 				<li>$100/1K Byggminuter</li>
-                <li>$0,03 / sida AI NLP översättningar</li>
+                <li>$0,30 / sida AI NLP översättningar</li>
                 <li>WebSockets och/eller värdtjänster för privata containrar tillgängliga</li>
                       </ol>
 					</p>
@@ -234,4 +234,4 @@ $ 1000 / månad, ~ 67 % rabatt på alla konkurrent prismodeller!
 h4 { font-size:14px }
 </style>
 
-<!-- $Date: 2026-03-25 13:28:05 -0700 (Wed, 25 Mar 2026) $ $Author: joe $ $Revision: 30211 $ -->
+<!-- $Date: 2026-03-27 16:12:44 -0700 (Fri, 27 Mar 2026) $ $Author: joe $ $Revision: 30523 $ -->
