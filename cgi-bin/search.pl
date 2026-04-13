53149e453e722f173c3813b3e7a398c204c3a610
(no author) <(no author)@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-04-13 13:31:37 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@31239 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -537,7 +537,7 @@ elsif ($repos and $re =~ /^([@\w.-]+=[@\w. -]*)$/i) {
       our $wlock :shared;
       lock($wlock) unless $wcache{"$svnuser-$url"};
       my ($watchers) = thaw($wcache{"$svnuser-$url"} ||= do {
-        $svn->update($dirname);
+        eval {$svn->update($dirname)};
         my $w = $svn->propget("orion:watchers", $dirname, undef, 1);
         $_ = {map {$_=>1} split /[, ]+/} for values %$w;
         while (my ($k, $v) = each %$w) {
