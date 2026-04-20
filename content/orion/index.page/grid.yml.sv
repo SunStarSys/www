32ce548ecf5c176f8fcf4d264763e2d6e40b0509
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-04-20 20:47:30 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@33748 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- content/orion/index.page/grid.yml.sv
+++ content/orion/index.page/grid.yml.sv
@@ -46,7 +46,7 @@ title: Tabelldata för rutnät för funktionsjämförelse
 - Feature: "Flerspråkig (i18n)"
   Orion: "&check;"
   Notion: "&check;"
-- Feature: Fullständigt sökbar/utbytbar <span class="editormd-tex">\KaTeX</span>/PCRE-uttryck"
+- Feature: 'Fullständigt sökbar/utbytbar <span class="editormd-tex">\KaTeX</span>/PCRE-uttryck'
   Orion: "&check;"
 - Feature: "Samordnad"
   Orion: "&check;"
@@ -94,7 +94,7 @@ title: Tabelldata för rutnät för funktionsjämförelse
 - Feature: "Sökbar dokumentstatus"
   Orion: "&check;"
   Slab: "&check;"
-- Feature: 'Nedsättning <span class="editormd-tex">\leftrightarrow \LaTeX</span> Automatisering
+- Feature: 'Nedsättning <span class="editormd-tex">\leftrightarrow \LaTeX</span> Automatisering'
   Orion: "&check;"
 - Feature: "Automatisk översättning av AI NLP"
   Orion: "&check;"
