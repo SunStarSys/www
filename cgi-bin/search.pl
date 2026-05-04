125b80f1bc7439f681e5e28ec06fadb431600866
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-05-04 15:49:34 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@35860 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -242,7 +242,7 @@ sub get_client_lang  (AR $r) {
 }
 
 my $markdown = $apreq->args("markdown_search") ? "Markdown" : "";
-my ($re)     = ($apreq->param("regex"))[-1] // ($r->status(Apache2::Const::HTTP_BAD_REQUEST) && return -1);
+my ($re)     = eval{($apreq->param("regex"))[-1]} // ($r->status(Apache2::Const::HTTP_BAD_REQUEST) && return -1);
 my $filter   = $apreq->param("filter") // "";
 my Digest::SHA1 $hash = $apreq->body("hash") // "";
 my $host     = $r->headers_in->{host};
