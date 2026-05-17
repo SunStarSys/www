287c5c0a22fb42828acf9217a2c907a601482977
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-05-17 12:07:12 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@37613 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -682,7 +682,7 @@ if ($re !~ $specials_re) {
       offset     => $apreq->param("offset") // 0,
       num_wanted => $apreq->param("wanted") // 100,
     );
-    my $hit_count = $hits->total_hits;
+    warn my $hit_count = $hits->total_hits;
     while (my $hit = $hits->next) {
 	   s/^\Q$path_info// or next for my $path = $hit->{path};
        push @{$matches{$path}}, {count => $hit->get_score, match => Template("{{content|truncatewords:10}}")->render($hits), pre => [], words => [], end => []};
