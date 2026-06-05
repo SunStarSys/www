e1d951dffbf820d7a33e10b8c2f4a8319a74fbb2
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-06-05 15:43:38 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@41666 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- content/orion/security.md.sv
+++ content/orion/security.md.sv
@@ -167,11 +167,11 @@ I ett nötskal är hur alla andra wiki-plattformar fungerar som en SQL [CRUD](ht
 
 I (noSQL) Orion tar vi redigering och rendering som två separata problem som ska hanteras av två oberoende programvarustackar; där mycket mer process, design, validering och beroendehantering investeras i redigeringsgränssnittet. Detta är så att vi kan begränsa programvaran på renderingsstacken till att vara en barebones SSI-aktiverad Apache-filserver med bog-standard webbautentisering och åtkomstkontroller inblandade, och optimistiskt förväntar vi oss att innehållet ses en storleksordning oftare än den redigeras.
 
-Följaktligen sker redigeringsupplevelsen på en omöjlig att komma åt-utan-en-valid-login oberoende webbstack, är lite mindre snabb när ändringarna har laddats upp, och är mycket mindre smutsig; eftersom vi validerar och bygger det modifierade innehållet, tillsammans med korpus av beroende sidor, *vid redigeringstid, inte vid renderingstid*. Byggnaden sker i en separat härdad, sandlådad Solaris Zone. Byggloggarna publiceras till redigeringssessionen i realtid och sparas i versionskontroll för eftervärlden och affärsansvar.
+Följaktligen sker redigeringsupplevelsen på en omöjlig att komma åt-utan-en-valid-login oberoende webbstack, är lite mindre snabb när ändringarna har laddats upp, och är mycket mindre smutsig; eftersom vi validerar och bygger det modifierade innehållet, tillsammans med hela korpus av beroende sidor, *vid redigeringstid, inte vid renderingstid*. Byggnaden sker i en separat härdad, sandlådad Solaris Zone. Byggloggarna publiceras till redigeringssessionen i realtid och sparas i versionskontroll för eftervärlden och affärsansvar.
 
-I själva verket är kostnaden några sekunder mer av exponering för byggets tekniska maskiner och statisk fildistributionsprocess innan man kan se bekräftade, publicerade ändringar på den levande webbplatsen.
+I själva verket är kostnaden några sekunder mer av exponering för byggets tekniska maskiner och statisk fildistributionsprocess innan man kan se engagerade, publicerade ändringar på den levande produktionswebbplatsen.
 
-Och förmånen? Du kommer **aldrig att se din webbplats bli hackad igen** genom nolldags- eller icke-korrigerade sårbarheter i den renderande webbserverns programvarustack.
+Och förmånen? Du kommer **aldrig se din live-webbplats bli hackad igen** genom nolldags- eller icke-korrigerade sårbarheter i den renderande webbserverns programvarustack. Dessutom är redigeringswebbplatsen **osårbar för anonyma hackningsinsatser**, så du är i en mycket bättre säkerhetsarkitektonisk position hos oss än med någon annan wiki-leverantör på jorden, oavsett de ytterligare skydd som implementeras i resten av detta dokument.
 
 Ett snyggt trick, eller hur?
 
