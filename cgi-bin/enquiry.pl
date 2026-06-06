67cc31e198fa2a5c46343d4c33283fc10bfa55a0
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-06-06 15:29:18 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@42487 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/enquiry.pl
+++ cgi-bin/enquiry.pl
@@ -112,7 +112,7 @@ if ($r->method eq "POST") {
   if ($vars{subject} =~ /$validator/i and ((defined $jar->get("nonce") and $jar->get("nonce") eq $body->get("nonce"))
   or (defined $jar->get("nonce2") and $jar->get("nonce2") eq $body->get("nonce")))) {
     s/^(.*)\@(.*)$/SRS0=999=99=$2=$1/, y/A-Za-z0-9._=-//dc for $srs_sender;
-    $srs_sender =~ /^(.*)$/ and length $1 or die "BAD EMAIL: $vars{email}";
+    $srs_sender =~ /^(.*)$/ and length $1 or do { warn "BAD EMAIL: $vars{email}" and $r->status(Apache2::Const::HTTP_BAD_REQUEST) and exit Apache2::Const::OK };
     %ENV = ();
 
     open my $sendmail, "|-", "/usr/sbin/sendmail", qw/-t -oi -odq -f/, "$1\@$DOMAIN";
