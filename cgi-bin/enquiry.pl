443ffee300fde8a4a6996d3b502707ee8ab03c96
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-06-06 03:55:48 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@42369 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/enquiry.pl
+++ cgi-bin/enquiry.pl
@@ -109,12 +109,13 @@ if ($r->method eq "POST") {
     }
   }
 
-  if ($vars{subject} =~ /$validator/i and defined $jar->get("nonce") and $jar->get("nonce") eq $body->get("nonce")) {
+  if ($vars{subject} =~ /$validator/i and ((defined $jar->get("nonce") and $jar->get("nonce") eq $body->get("nonce"))
+  or (defined $jar->get("nonce2") and $jar->get("nonce2") eq $body->get("nonce")))) {
     s/^(.*)\@(.*)$/SRS0=999=99=$2=$1/, y/A-Za-z0-9._=-//dc for $srs_sender;
     $srs_sender =~ /^(.*)$/ and length $1 or die "BAD EMAIL: $vars{email}";
     %ENV = ();
 
-    open my $sendmail, "|-", "/usr/sbin/sendmail", qw/-t -oi -f/, "$1\@$DOMAIN";
+    open my $sendmail, "|-", "/usr/sbin/sendmail", qw/-t -oi -odq -f/, "$1\@$DOMAIN";
 
     my $msg =<<EOT;
 To: $to
