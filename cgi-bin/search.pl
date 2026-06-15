b409f7c3ebc9bd1a916a91246c1810fd1ebd3c7c
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-06-15 22:12:08 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@45885 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -322,11 +322,11 @@ if ($repos and $re =~ /^weblog=(.*)/i) {
     $s = $s->new;
     $s->permit_only(@opcodes);
     eval {
-      $s->reval(qr{\Q$prefix\E}) if $prefix;
+      $s->reval(q{/\Q$prefix\E/}) if $prefix;
       die $@ if $@;
-      $s->reval(qr{$prefilter}) if $prefilter;
+      $s->reval(q{/$prefilter/}) if $prefilter;
       die $@ if $@;
-      $s->reval(qr{$filter}) if $filter;
+      $s->reval(q{/$filter/}) if $filter;
       die $@ if $@;
     };
 
@@ -722,9 +722,9 @@ if ($re !~ $specials_re) {
   $s = $s->new;
   $s->permit_only(@opcodes);
   eval {
-    $s->reval(qr{$re}) if $re;
+    $s->reval(q{/$re/}) if $re;
     die $@ if $@;
-    $s->reval(qr{$filter}) if $filter;
+    $s->reval(q{/$filter/}) if $filter;
     die $@ if $@;
   };
   $r->status(Apache2::Const::HTTP_BAD_REQUEST), return Apache2::Const::HTTP_BAD_REQUEST if $@;
