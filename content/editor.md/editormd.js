80c0b62c8bb896844930740a2355539086116727
joe <joe@8bcbd36e-e7ad-eafc-85f0-e656ad0bf263>
2026-06-28 15:43:24 +0000

    Orion commit to www.sunstarsys.com by joe@sunstarsys.com
    git-svn-id: https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk@46466 8bcbd36e-e7ad-eafc-85f0-e656ad0bf263

--- content/editor.md/editormd.js
+++ content/editor.md/editormd.js
@@ -155,7 +155,7 @@
         onpreviewscroll      : function() {},
 
         imageUpload          : false,
-        imageFormats         : ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
+        imageFormats         : ["jpg", "jpeg", "gif", "png", "bmp", "webp", "svg"],
         imageUploadURL       : "",
         crossDomainUpload    : false,
         uploadCallbackURL    : "",
@@ -543,7 +543,10 @@
 
                 if (settings.mermaid)
                 {
-                    _this.editormd.loadCSS(loadPath + "../css/mermaid.min", function () {
+		    if (typeof mermaid !== "undefined")
+			_this.editormd.$mermaid = mermaid;
+		    else
+                      _this.editormd.loadCSS(loadPath + "../css/mermaid.min", function () {
                         _this.editormd.loadScript(loadPath + "mermaid.min", async function () {
                             //_this.editormd.loadScript(loadPath + "mermaid-mindmap.min", function () {
                                 _this.editormd.$mermaid = mermaid;
@@ -3536,6 +3539,7 @@
      * @returns {Renderer} markedRenderer  返回marked的Renderer自定义对象
      */
 
+    const xcache = new Map();
     editormd.markedRenderer = function(markdownToC, options) {
         var defaults = {
             toc                  : true,           // Table of contents
@@ -3565,7 +3569,8 @@
         var faIconReg       = regexs.fontAwesome;
         var editormdLogoReg = regexs.editormdLogo;
         var pageBreakReg    = regexs.pageBreak;
-
+        var slugIndex       = 0;
+	
         markedRenderer.emoji = function(text) {
 
             text = text.replace(editormd.regexs.emojiDatetime, function($1) {
@@ -3612,7 +3617,7 @@
                         }
                         else if (camelemojiMatchs)
                         {
-                            for (var t = 0, len3 = camelemojiMatchs.length; t < sluglen3; t++)
+                            for (var t = 0, len3 = camelemojiMatchs.length; t < len3; t++)
                             {
                                 var twe = camelemojiMatchs[t].replace(/:/g, "");
                                 return "<img src=\"" + editormd.camel_emoji.path + twe + editormd.camel_emoji.ext + "\" title=\"camel-emoji-" + twe + "\" alt=\"camel-emoji-" + twe + "\" class=\"emoji camel-emoji\" />";
@@ -3634,8 +3639,17 @@
         };
 
         markedRenderer.atLink = function(text) {
-
-            if (atLinkReg.test(text))
+	    for (const m of text.matchAll(atLinkReg))
+		if (settings.atLink && /\/status\//.test(m[1]) && !xcache.get(m[1])) {
+         	    var url = "https://publish.x.com/oembed?url=https://x.com/" + m[1] + "&dnt=1";
+		    !async function () {
+			var response = await fetch(url);
+			if (!response.ok)
+			    return;
+			return await response.json();
+		    }().then((json)=>{ xcache.set(m[1], json.html);});
+		}
+	    if (atLinkReg.test(text))
             {
                 if (settings.atLink)
                 {
@@ -3690,7 +3704,12 @@
                         }
                         else if (/=$/.test($b))
                             return "<a href=\"" + editormd.urls.atLinkBase + "" + $b + "\" title=\"&#64;" + $b + "\" class=\"at-link\">" + $a + "</a>";
-                        else return "<a href=\"https://github.com/" + $b + "\" title=\"&64;" + $b + "\" class=\"at-link\">" + $a + "</a>";
+			else if (/\/status\//.test($b)) {
+			    if (xcache.get($b))
+				return xcache.get($b);
+			    return $a;
+			}
+                        else return "<a href=\"https://github.com/" + $b + "\" title=\"@" + $b + "\" class=\"at-link\">" + $a + "</a>";
                     }).replace(/_#_&#64;_#_/g, "@");
                 }
 
@@ -3772,7 +3791,7 @@
             var toc = {
                 text  : text,
                 level : level,
-                slug  : escapedText
+                slug  : escapedText + ++slugIndex
             };
 
 
@@ -3780,7 +3799,7 @@
 
             var headingHTML = "<h" + level + " id=\"h"+ level + "-" + this.options.headerPrefix + id +"\">";
 
-            headingHTML    += "<a name=\"" + escapedText + "\" class=\"reference-link\"></a>";
+            headingHTML    += "<a name=\"" + escapedText + slugIndex + "\" class=\"reference-link\"></a>";
             headingHTML    += "<span class=\"header-link octicon octicon-link\"></span>";
             headingHTML    += (hasLinkReg) ? this.atLink(this.emoji(linkText)) : this.atLink(this.emoji(text));
             headingHTML    += "</h" + level + ">";
@@ -4415,8 +4434,8 @@
         css : "/editor.md/lib/katex.min",
         js  : "/editor.md/lib/katex.min",
         physics: "/editor.md/lib/katex-physics",
-        mhchem: "/editor.md/lib/mhchem",
-        copy: "/editor.md/lib/copy-tex"
+        mhchem: "/editor.md/lib/mhchem.min",
+        copy: "/editor.md/lib/copy-tex.min"
     };
 
     editormd.kaTeXLoaded = false;
